7d7ce157c6a7b8e0081cd5863a71a57c3f568463
lileilei12 <126468180+lileilei12@users.noreply.github.com>
2025-02-10 23:05:41 +0800

    test.py

--- test.py
+++ test.py
@@ -14,20 +14,20 @@ from DrtNet_FA_Model import DrtNet_FA as kidney_seg
 from DrtNet_FA_Model import CONFIGS as CONFIGS_seg
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--volume_path', type=str, default=r"C:\FOLDER\code_project\EX\All_Data\npz_tumor", help='root dir for validation volume data')
+parser.add_argument('--volume_path', type=str, default='./All_Data/organ_npz', help='root dir for validation volume data')
+parser.add_argument('--list_dir', type=str, default='./All_Data/list', help='list dir')
+parser.add_argument('--test_save_dir', type=str, default='./result', help='saving prediction as nii!')
 parser.add_argument('--dataset', type=str, default='Synapse', help='experiment_name')
 parser.add_argument('--num_classes', type=int, default=2, help='output channel of network')
-parser.add_argument('--list_dir', type=str, default=r"C:\FOLDER\code_project\EX\All_Data\list\tumor\proving", help='list dir')
-parser.add_argument('--max_iterations', type=int,default=20000, help='maximum epoch number to train')
-parser.add_argument('--max_epochs', type=int, default=80, help='maximum epoch number to train')
+parser.add_argument('--max_iterations', type=int, default=30000, help='maximum epoch number to train')
+parser.add_argument('--max_epochs', type=int, default=20, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=4, help='batch_size per gpu')
 parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
-parser.add_argument('--is_savenii', type=bool,default=True, help='whether to save results during inference')
+parser.add_argument('--is_savenii', type=bool, default=True, help='whether to save results during inference')
 parser.add_argument('--n_layer', type=int, default=4, help='the number of ResNet layers, default is four')
-parser.add_argument('--vit_name', type=str, default='R50-ViT-B_16', help='select one vit model')
-parser.add_argument('--test_save_dir', type=str, default='../predictions', help='saving prediction as nii!')
-parser.add_argument('--deterministic', type=int,  default=1, help='whether use deterministic training')
-parser.add_argument('--base_lr', type=float,  default=1e-3, help='segmentation network learning rate')
+parser.add_argument('--vit_name', type=str, default='Config', help='select one vit model')
+parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
+parser.add_argument('--base_lr', type=float, default=1e-3, help='segmentation network learning rate')
 parser.add_argument('--seed', type=int, default=1234, help='random seed')
 parser.add_argument('--patch_size', type=int, default=32, help='patch_size, default is 32')
 args = parser.parse_args()
@@ -40,7 +40,7 @@ def inference(args, model, test_save_path=None):
     model.eval()
     metric_list = 0.0
     for i_batch, sampled_batch in tqdm(enumerate(testloader)):
-        
+
         image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
         metric_i = test_single_volume(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
                                       test_save_path=test_save_path, case=case_name, z_spacing=args.z_spacing)
@@ -49,7 +49,7 @@ def inference(args, model, test_save_path=None):
     a = metric_list
     metric_list = metric_list / len(db_test)
     for i in range(1, args.num_classes):
-        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i-1][0], metric_list[i-1][1]))
+        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i - 1][0], metric_list[i - 1][1]))
     performance = np.mean(metric_list, axis=0)[0]
     mean_hd95 = np.mean(metric_list, axis=0)[1]
     logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
@@ -71,9 +71,9 @@ if __name__ == "__main__":
     dataset_config = {
         'Synapse': {
             'Dataset': Synapse_dataset,
-            'volume_path': r"C:\FOLDER\code_project\EX\All_Data\npz_tumor",
-            'list_dir': r"C:\FOLDER\code_project\EX\All_Data\list\tumor\proving",
-            'num_classes': 2,
+            'volume_path': args.volume_path,
+            'list_dir': args.list_dir,
+            'num_classes': args.num_classes,
             'z_spacing': 1,
         },
     }
@@ -84,44 +84,42 @@ if __name__ == "__main__":
     args.list_dir = dataset_config[dataset_name]['list_dir']
     args.z_spacing = dataset_config[dataset_name]['z_spacing']
     args.is_pretrain = True
-
     args.exp = 'TU_' + dataset_name + str(args.img_size)
-    snapshot_path = "C:/FOLDER/code_project/EX/FKTS4/model_tumor/{}/{}".format(args.exp, 'TU')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    model_dir = os.path.join(script_dir, "model")
+    snapshot_path = os.path.join(model_dir, args.exp, 'TU')
     snapshot_path = snapshot_path + '_pretrain' if args.is_pretrain else snapshot_path
     snapshot_path += '_' + args.vit_name
     snapshot_path = snapshot_path + '_layer' + str(args.n_layer)
-    snapshot_path = snapshot_path + '_vitpatch' + str(args.patch_size) if args.patch_size!=16 else snapshot_path
+    snapshot_path = snapshot_path + '_vitpatch' + str(args.patch_size) if args.patch_size != 16 else snapshot_path
     snapshot_path = snapshot_path + '_' + str(args.max_iterations)[0:2] + 'k' if args.max_iterations != 30000 else snapshot_path
     snapshot_path = snapshot_path + '_epo' + str(args.max_epochs) if args.max_epochs != 30 else snapshot_path
-    snapshot_path = snapshot_path+'_bs'+str(args.batch_size)
+    snapshot_path = snapshot_path + '_bs' + str(args.batch_size)
     snapshot_path = snapshot_path + '_lr' + str(args.base_lr) if args.base_lr != 0.01 else snapshot_path
-    snapshot_path = snapshot_path + '_'+str(args.img_size)
-    snapshot_path = snapshot_path + '_s'+str(args.seed) if args.seed!=1234 else snapshot_path
-     
+    snapshot_path = snapshot_path + '_' + str(args.img_size)
+    snapshot_path = snapshot_path + '_s' + str(args.seed) if args.seed != 1234 else snapshot_path
+
     config_vit = CONFIGS_seg[args.vit_name]
     config_vit.n_classes = args.num_classes
     config_vit.n_layer = args.n_layer
-    config_vit.patches.size = (args.patch_size, args.patch_size)
-    net = kidney_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
-
+    config_vit.patch_size = args.patch_size
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = kidney_seg(config_vit, img_size=args.img_size).to(device)
     snapshot = os.path.join(snapshot_path, 'best_model.pth')
-    if not os.path.exists(snapshot): snapshot = snapshot.replace('best_model', 'epoch_'+str(args.max_epochs-1))
-    net.load_state_dict(torch.load(snapshot))
-    snapshot_name = snapshot_path.split('/')[-1]
-
+    if not os.path.exists(snapshot):
+        snapshot = snapshot.replace('best_model', 'epoch_' + str(args.max_epochs - 1))
+    net.load_state_dict(torch.load(snapshot, map_location=device))
+    snapshot_name = snapshot_path.split(os.sep)[-1]
     log_folder = './test_log/test_log_' + args.exp
     os.makedirs(log_folder, exist_ok=True)
-    logging.basicConfig(filename=log_folder + '/'+snapshot_name+".txt", level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.basicConfig(filename=log_folder + '/' + snapshot_name + ".txt", level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
     logging.info(str(args))
     logging.info(snapshot_name)
 
     if args.is_savenii:
-        args.test_save_dir = r"C:\FOLDER\code_project\EX\KTS\result"
         test_save_path = os.path.join(args.test_save_dir, args.exp, snapshot_name)
         os.makedirs(test_save_path, exist_ok=True)
     else:
         test_save_path = None
     inference(args, net, test_save_path)
-
-
